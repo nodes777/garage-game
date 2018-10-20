be7d7128cec6f4ecdedad4bd6293dfe50bb7353a
Taylor <taylor.nodell@gmail.com>
2018-10-20 15:58:53 +0800

    inital commit

---
+++ Assets/ActivatePulley.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePulley : MonoBehaviour {
+
+    public PulleyScript PulleyScript;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // declare a Raycast hit
+            RaycastHit hit;
+            // define the ray as the mouse pos in the direction of the camera
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // take in the ray, and the hit, but make sure you output the hit information into the hit, and 100f is the distance to check
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log(hit.transform.gameObject);
+                if (hit.transform == transform)
+                {
+                    PulleyScript.enabled = true;
+                }
+            }
+        }
+
+    }
+}
